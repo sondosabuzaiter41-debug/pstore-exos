fe890128cbd764840e8a77d722a22e75afc1881e
sondos <sondosabuzaiter41@gmail.com>
2026-04-30 19:17:20 +0300

    Update comments in hello.php

--- hello.php
+++ hello.php
@@ -6,8 +6,10 @@ $name = "Sondos";
 echo "Bonjour " . $name;
 // Task 1.4
 //  command
-//  ls=> Not work in windows (لكن البديل بنستخدم dir ووظيفتهابتعرض الملفات اللي جوا المكان الحالي)
+//  ls=> Not working on cmd but working on powershell (    ووظيفتهابتعرض كل الملفات اللي جوا المكان الحالي)
 //  cd .. => Go back one folder (بيرجعنا فولدر لورا )
 //  cd pstore-exos => Enter a specific folder that name pstpre-exos (يعني بدخلنا جوا فولدر اسمه pstore-exos)
-//  clear=> Not work in windows (لكن البديل cls ووظيفتها بتعمل تنظيف لشاشة التيرمنال)
+//  clear=> Not working  on cmd but working on powershell
+//  and the alternative is cls on cmd
+//  ( ووظيفتها بتعمل تنظيف لشاشة التيرمنال)
 ?>
